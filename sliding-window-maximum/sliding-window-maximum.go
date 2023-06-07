ae211126f39a6b3b3e1ccdbc5bb221eb5e5e3c4b
kappaprideonly <kolmanoney@gmail.com>
2023-06-07 20:39:08 +0300

    sliding window

---
+++ sliding-window-maximum/sliding-window-maximum.go
@@ -0,0 +1,64 @@
+package slidingwindowmaximum
+
+type Deque struct {
+	list []int
+}
+
+func newDeque() Deque {
+	return Deque{list: []int{}}
+}
+
+func (d *Deque) Push(x int) {
+	d.list = append(d.list, x)
+}
+
+func (d *Deque) Len() int {
+	return len(d.list)
+}
+
+func (d *Deque) PopFront() {
+	d.list = d.list[1:]
+}
+
+func (d *Deque) PopBack() {
+	n := len(d.list)
+	d.list = d.list[:n-1]
+}
+
+func (d *Deque) Front() int {
+	return d.list[0]
+}
+
+func (d *Deque) Back() int {
+	return d.list[len(d.list)-1]
+}
+
+func maxSlidingWindow(nums []int, k int) []int {
+	ans := []int{}
+	d := newDeque()
+	for i := 0; i < k; i++ {
+		for d.Len() > 0 && d.Back() <= nums[i] {
+			d.PopBack()
+		}
+		d.Push(nums[i])
+	}
+	ans = append(ans, max(d.Front(), d.Back()))
+	for i := k; i < len(nums); i++ {
+		if d.Front() == nums[i-k] {
+			d.PopFront()
+		}
+		for d.Len() > 0 && d.Back() <= nums[i] {
+			d.PopBack()
+		}
+		d.Push(nums[i])
+		ans = append(ans, max(d.Front(), d.Back()))
+	}
+	return ans
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
